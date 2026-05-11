9e73017051c0ae582f33c2344bfd2bb8f1cad981
koncc1 <volk81498@gmail.com>
2026-05-11 19:01:47 +0200

    Create ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TravelPlannerMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
