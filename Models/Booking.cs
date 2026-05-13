117a68aa0a21231757f10eeebd4d429da060532b
koncc1 <volk81498@gmail.com>
2026-05-13 19:44:59 +0200

    Add models "Route , User, Booking"

---
+++ Models/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelPlannerMVC.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int RouteId { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int SeatsCount { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Pending";
+
+        public User User { get; set; }
+
+        public Route Route { get; set; }
+    }
+}
---
+++ Models/Route.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelPlannerMVC.Models
+{
+    public class Route
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string StartCity { get; set; } = string.Empty;
+
+        [Required]
+        public string EndCity { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime DepartureDate { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int AvailableSeats { get; set; }
+    }
+}
