6d5716d5734a0019f63f6144315d199c2470461f
koncc1 <volk81498@gmail.com>
2026-05-21 18:26:11 +0200

    Login page was created, program.cs was reworked

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using TravelPlannerMVC.Data;
 using TravelPlannerMVC.Models;
 using TravelPlannerMVC.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 namespace TravelPlannerMVC.Controllers
 {
@@ -14,6 +17,8 @@ namespace TravelPlannerMVC.Controllers
         {
             db = context;
         }
+
+        #region REGISTER
 
         [HttpGet]
         public IActionResult Register()
@@ -46,12 +51,74 @@ namespace TravelPlannerMVC.Controllers
             PasswordHasher<User> hasher = new PasswordHasher<User>();
             newUser.PasswordHash = hasher.HashPassword(newUser, model.Password);
 
-
-
             db.Users.Add(newUser);
             db.SaveChanges();
 
             return RedirectToAction("Index", "Home");
         }
+
+        #endregion
+
+        #region LOGIN
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+
+            User user = db.Users.FirstOrDefault(x => x.Email == model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Invalid email or password");
+                return View(model);
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+
+            PasswordVerificationResult passwordResult = hasher.VerifyHashedPassword(
+                user,
+                user.PasswordHash,
+                model.Password
+            );
+
+            if (passwordResult == PasswordVerificationResult.Failed)
+            {
+                ModelState.AddModelError("", "Invalid email or password");
+                return View(model);
+            }
+
+            List<Claim> userInfo = new List<Claim>();
+
+            userInfo.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+            userInfo.Add(new Claim(ClaimTypes.Name, user.Name));
+            userInfo.Add(new Claim(ClaimTypes.Email, user.Email));
+            userInfo.Add(new Claim(ClaimTypes.Role, user.Role));
+
+            ClaimsIdentity userIdentity = new ClaimsIdentity(
+                userInfo,
+                CookieAuthenticationDefaults.AuthenticationScheme
+            );
+
+            ClaimsPrincipal userPrincipal = new ClaimsPrincipal(userIdentity);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                userPrincipal
+            );
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        #endregion
     }
 }
