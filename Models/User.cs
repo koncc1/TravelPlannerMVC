900755dfa1202eb7a7867a124330ae6f49e7bf09
koncc1 <volk81498@gmail.com>
2026-05-21 17:33:35 +0200

    Model rework !

--- Models/User.cs
+++ Models/User.cs
@@ -7,14 +7,14 @@ namespace TravelPlannerMVC.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
 
         [Required]
-        public string PasswordHash { get; set; }
+        public string PasswordHash { get; set; } = string.Empty;
 
         [Required]
         public string Role { get; set; } = "User";
