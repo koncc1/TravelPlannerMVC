d66a30db4c4bac71ec1dc41af7aa4bea582eb312
koncc1 <volk81498@gmail.com>
2026-05-27 12:43:24 +0200

    DTOs was added

---
+++ DTOs/TravelRequestCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelPlannerMVC.DTO
+{
+    public class TravelRequestCreateDto
+    {
+        public int UserId { get; set; }
+        public string StartCity { get; set; }
+        public string EndCity { get; set; }
+        public DateTime PreferredDate { get; set; }
+    }
+}
