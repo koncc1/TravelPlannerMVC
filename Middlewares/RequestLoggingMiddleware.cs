214f732574b9c8cdef83d2ed7ca7eb05439977fd
koncc1 <volk81498@gmail.com>
2026-05-27 14:01:20 +0200

    Middlewares added

---
+++ Middlewares/RequestLoggingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace TravelPlannerMVC.Middleware
+{
+    public class AuthCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthCheckMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var path = context.Request.Path.ToString().ToLower();
+
+            if (path.Contains("/account/login") || path.Contains("/account/register"))
+            {
+                await _next(context);
+                return;
+            }
+
+            if (context.User?.Identity == null || !context.User.Identity.IsAuthenticated)
+            {
+                context.Response.Redirect("/Account/Login");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
